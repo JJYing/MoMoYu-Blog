06e97013777ab2b96cb54d52fde7f855e7c1b0af
JJ Ying <yingjunjiu@gmail.com>
2025-12-05 23:17:44 +0800

    更新了文章、增加了统计代码，加了字体 CSS

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -5,10 +5,30 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
+        <link rel="preconnect" href="https://s.anyway.red" crossorigin/>
+        <link rel="icon" href="https://s.anyway.red/momoyu/favicon.png">
+        <link rel="stylesheet" href="https://s.anyway.red/momoyu/fontawesome.css">
+        <link rel="stylesheet" href="https://s.anyway.red/font/alihealth2/regular/regular.css">
+        <link rel="stylesheet" href="https://s.anyway.red/font/alihealth2/bold/bold.css">
+        
         <title>{{ $page->title }}</title>
         @viteRefresh()
         <link rel="stylesheet" href="{{ vite('source/_assets/css/main.css') }}">
         <script defer type="module" src="{{ vite('source/_assets/js/main.js') }}"></script>
+        <script>
+            var _paq = window._paq = window._paq || [];
+            _paq.push(["setCookieDomain", "*.momoyu.app"]);
+            _paq.push(["setDomains", ["*.momoyu.app"]]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//anyway.fm/matomo/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '5']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          </script>        
     </head>
     <body class="bg-slate-50 text-gray-900 font-sans antialiased">
         @yield('body')
